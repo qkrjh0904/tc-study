602fe88fa1e636f8c093fe586208c610b0cdc263
jeongho <qkrjh0904@gmail.com>
2023-07-19 22:01:03 +0900

    feat: Item Unit Test

--- src/test/java/com/example/tc/unit/item/CupTest.java
+++ src/test/java/com/example/tc/unit/item/CupTest.java
@@ -8,11 +8,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CupTest {
 
     @Test
-    @DisplayName("Bag 단위 테스트")
-    void test() {
+    @DisplayName("컵 유닛 테스트")
+    void cupUnitTest() {
         Cup cup = new Cup();
+
         assertThat(cup.getPrice()).isEqualTo(1000);
         assertThat(cup.getName()).isEqualTo("컵");
     }
-
 }
