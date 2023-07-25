d258a81003fec0f9386aa0861067a16c06a77db4
jeongho <qkrjh0904@gmail.com>
2023-07-25 22:28:16 +0900

    feat: Persistence Layer test code 작성

--- src/main/java/com/tc/spring/domain/item/repository/ItemRepository.java
+++ src/main/java/com/tc/spring/domain/item/repository/ItemRepository.java
@@ -1,7 +1,12 @@
 package com.tc.spring.domain.item.repository;
 
 import com.tc.spring.db.entity.Item;
+import com.tc.spring.db.enums.SellingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findBySellingStatusIn(List<SellingStatus> sellingStatusList);
 }
--- src/test/java/com/tc/spring/domain/item/repository/ItemRepositoryTest.java
+++ src/test/java/com/tc/spring/domain/item/repository/ItemRepositoryTest.java
@@ -3,6 +3,7 @@ package com.tc.spring.domain.item.repository;
 import com.tc.spring.db.entity.Item;
 import com.tc.spring.db.enums.ItemType;
 import com.tc.spring.db.enums.SellingStatus;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,11 @@ class ItemRepositoryTest {
 
     @Autowired
     private ItemRepository itemRepository;
+
+    @AfterEach
+    void tearDown() {
+        itemRepository.deleteAllInBatch();
+    }
 
     @Test
     @DisplayName("상품을 등록할 수 있다.")
@@ -42,5 +48,26 @@ class ItemRepositoryTest {
                 );
     }
 
+    @Test
+    @DisplayName("판매 상태로 아이템을 찾아올 수 있다.")
+    void findBySellingStatusIn() {
+        // given
+        Item bag1 = Item.create("001", "샤넬백", 4_500_000, ItemType.BAG, SellingStatus.SELLING);
+        Item bag2 = Item.create("002", "루이비똥백", 5_300_000, ItemType.BAG, SellingStatus.SOLD_OUT);
+        Item bag3 = Item.create("003", "에르메스백", 9_900_000, ItemType.BAG, SellingStatus.STOP_SELLING);
+        Item bag4 = Item.create("004", "구찌백", 3_200_000, ItemType.BAG, SellingStatus.STOP_SELLING);
+        itemRepository.saveAll(List.of(bag1, bag2, bag3, bag4));
+
+        // when
+        List<Item> list = itemRepository.findBySellingStatusIn(List.of(SellingStatus.STOP_SELLING));
+
+        // then
+        assertThat(list).hasSize(2)
+                .extracting("itemNumber", "name", "price", "itemType", "sellingStatus")
+                .containsExactlyInAnyOrder(
+                        tuple("003", "에르메스백", 9_900_000, ItemType.BAG, SellingStatus.STOP_SELLING),
+                        tuple("004", "구찌백", 3_200_000, ItemType.BAG, SellingStatus.STOP_SELLING)
+                );
+    }
 
 }
